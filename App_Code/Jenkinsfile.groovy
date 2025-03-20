83e8e844f98a7eda42badfdc3de61feed34b48c3
PartH-A11 <121934544+PartH-A11@users.noreply.github.com>
2025-03-20 19:38:04 +0530

    Update Jenkinsfile.groovy
    Change 1

--- App_Code/Jenkinsfile.groovy
+++ App_Code/Jenkinsfile.groovy
@@ -1,18 +1,12 @@
 pipeline {
     agent any
-    environment {
-        APEX_WORKSPACE = "edc_jcdc_dev"
-        APEX_USERNAME = "parth.suthar"
-        DB_HOST = "13.203.90.85"
-        DB_SERVICE = "OSPROD"
-    }
     stages {
         stage('Checkout Code') {
             steps {
                 script {
                     checkout([
                         $class: 'GitSCM',
-                        branches: [[name: 'main']],
+                        branches: [[name: 'develop']],
                         userRemoteConfigs: [[
                             url: 'https://github.com/PartH-A11/oct_apex_repository.git',
                             credentialsId: 'github-creds'
